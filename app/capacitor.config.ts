ed0760099af82c4f721abdcdfc06d2918505ebf3
Stefan T <stefan.tudose@colossusbets.com>
2023-01-19 14:39:57 +0200

    Scaffold Capacitor app

---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.colossusbets.testapp',
+  appName: 'CB Test',
+  webDir: './dist',
+  bundledWebRuntime: false
+};
+
+export default config;
